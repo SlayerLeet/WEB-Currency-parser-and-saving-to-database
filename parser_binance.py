1e13267655f8854a76dc7f1540fb60462d6fb726
SlayerLeet <zhuchkov337@gmail.com>
2025-10-17 18:06:56 +0300

    the server is disabled, and the client retrieves data directly from the parsers. The logic for adding records to the database is also configured

--- parser_binance.py
+++ parser_binance.py
@@ -5,4 +5,3 @@ def cripta():
     formatted_string_ru = now.strftime("%d.%m.%Y %H:%M")
     cripta_dict["USDT"] = {"price" : 100, "date" : formatted_string_ru}
     return cripta_dict
-print(cripta())
--- web_client.py
+++ web_client.py
@@ -1,13 +1,13 @@
 from flask import Flask, url_for, render_template
 app = Flask(__name__)
 from parser_moex import moex
-import requests
+from parser_binance import cripta
 from datetime import datetime
 
 
 @app.route("/")
 def index():
-    values = requests.get("http://127.0.0.1:5000/api/main").json()
+    values = {"fiat" : moex(), "cripta" : cripta()}
     return render_template("index.html", values = values )
 
 if __name__=="__main__":
