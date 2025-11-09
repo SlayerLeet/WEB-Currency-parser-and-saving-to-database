fab6d43205d7859d9b9e26b5a8842f81e698432c
SlayerLeet <zhuchkov337@gmail.com>
2025-11-09 15:00:28 +0300

    add parsing from Binance

--- parser.py
+++ parser.py
@@ -2,16 +2,20 @@ import requests
 from json import loads
 from datetime import datetime
 
-# Ссылки на валюты:
-btc_url = 'https://www.okx.com/api/v5/public/mark-price?instId=BTC-USDT-SWAP'
-eth_url = 'https://www.okx.com/api/v5/public/mark-price?instId=ETH-USDT-SWAP'
-bnb_url = 'https://www.okx.com/api/v5/public/mark-price?instId=BNB-USDT-SWAP'
+#------------------------------------------------------------------------------------------------------------------------------------------------
+# OKX
 
 # Базовые данные:
 result_dict = {}
 
-# Парсинг с okx:
 def okx_parse():
+    
+    # Ссылки на валюты:
+    btc_url = 'https://www.okx.com/api/v5/public/mark-price?instId=BTC-USDT-SWAP'
+    eth_url = 'https://www.okx.com/api/v5/public/mark-price?instId=ETH-USDT-SWAP'
+    bnb_url = 'https://www.okx.com/api/v5/public/mark-price?instId=BNB-USDT-SWAP'
+    
+    # Парсинг с okx:
     for url in [btc_url, eth_url, bnb_url]:
         req = requests.get(url).text
         
@@ -27,12 +31,43 @@ def okx_parse():
         result_dict[name] = {"price" : price, "date" : formatted_string_ru}
         
     return result_dict
-    
-print(result_dict)
-
 
 # Валюты которые парсю
 
 # BTC-USDT-SWAP
 # ETH-USDT-SWAP
 # BNB-USDT-SWAP
+#------------------------------------------------------------------------------------------------------------------------------------------------
+
+# BINANCE
+
+# Базовые данные:
+binance_dict = {}
+
+
+def binance_parse():
+    
+    # Ссылки на валюты:
+    btc_url = 'https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT'
+    eth_url = 'https://api.binance.com/api/v3/avgPrice?symbol=ETHUSDT'
+    bnb_url = 'https://api.binance.com/api/v3/avgPrice?symbol=BNBUSDT'
+    
+    # Парсинг с okx:
+    for url, name in zip([btc_url, eth_url, bnb_url], ['BTC/USDT','ETH/USDT','BNB/USDT']):
+        req = requests.get(url).text
+        
+        price = loads(req).get("price")
+
+        now = datetime.now()
+        formatted_string_ru = now.strftime("%d.%m.%Y %H:%M")
+        binance_dict[name] = {"price" : price, "date" : formatted_string_ru}
+        
+    return binance_dict
+
+print(okx_parse())
+print(binance_parse())
+
+# BTC-USDT
+# ETH-USDT
+# BNB-USDT
+#------------------------------------------------------------------------------------------------------------------------------------------------
--- parser_binance.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-def cripta():
-    cripta_dict = {}
-    now = datetime.now()
-    formatted_string_ru = now.strftime("%d.%m.%Y %H:%M")
-    cripta_dict["USDT"] = {"price" : 100, "date" : formatted_string_ru}
-    return cripta_dict
